8bd849124aada6c36ec7ff9c5597e3b361f4d099
JustAnotherDevFromLA <artasheskocharyan@gmail.com>
2023-02-04 12:00:32 -0800

    Updating Comments

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,6 @@
-/*
-	Prologue by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
